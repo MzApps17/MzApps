7505aef2f79704adde06727a452dc0c16d6b141d
MzApps17 <mizochatapps@gmail.com>
2026-08-25 22:59:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 import './globals.css'
+import Footer from '../components/Footer'
 
 export const metadata = {
-  title: 'MzApps - Mizo Chat App',
-  description: 'Mizoram tan',
+  title: 'MzApps',
+  description: 'Mizo Social App',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{background:'#000', color:'#fff', paddingBottom:'70px'}}>
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
