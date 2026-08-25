9020c41eb306d59356c73c4a2870974cd12cca6a
MzApps17 <mizochatapps@gmail.com>
2026-08-25 23:38:47 +0530

    Create status.tsx

---
+++ app/status.tsx
@@ -0,0 +1,7 @@
+'use client'
+export default function StatusPage(){
+  return <div style={{padding:'20px', paddingBottom:'90px', background:'#fff', minHeight:'100vh'}}>
+    <div style={{fontWeight:'900', fontSize:'22px'}}>Status ⭕</div>
+    <div style={{marginTop:'20px', textAlign:'center', fontWeight:'700', color:'#999'}}>Status la awm lo</div>
+  </div>
+}
