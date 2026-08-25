d918b3a7631d30225592051f3c659aeb08e7608e
MzApps17 <mizochatapps@gmail.com>
2026-08-25 22:44:08 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh', flexDirection:'column'}}>
+      <h1 style={{fontSize:'50px', fontWeight:'bold'}}>MzApps 🔥</h1>
+      <p style={{fontSize:'20px', color:'#aaa'}}>Mizo App Store - Setup Zo ta!</p>
+      <p style={{marginTop:'20px', background:'green', padding:'10px 20px', borderRadius:'20px'}}>Vercel ah Deploy theih tawh!</p>
+    </main>
+  )
+}
