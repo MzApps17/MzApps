8eada36454475998ec9cddba00822bde3a6bb60f
MzApps17 <mizochatapps@gmail.com>
2026-08-28 12:35:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
-export default function RootLayout({children}:{children:React.ReactNode}){
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MZ Apps",
+  description: "Mizoram Bazar & Jobs",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-white text-black">{children}</body>
     </html>
   );
 }
