797a8a79831aa091251785c9c3d199dd95235449
MzApps17 <mizochatapps@gmail.com>
2026-08-30 22:46:01 +0530

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -15,7 +15,8 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon-192x192.png'
+    icon: '/icon-192.png',
+    badge: '/icon-192.png'
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
